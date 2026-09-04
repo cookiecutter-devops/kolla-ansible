104fc4aa440bf33d07908a71b4c53ca55dca920b
hujianli94 <1879324764@qq.com>
2026-09-04 18:14:03 +0800

    add library

---
+++ ansible/library/dynamic_defaults.py
@@ -0,0 +1,115 @@
+import re
+
+from ansible.module_utils.basic import AnsibleModule
+
+LOOKUP_KEYS = [
+    '{ansible_distribution}-{ansible_distribution_version}',
+    '{ansible_distribution}',
+    '{ansible_system}',
+    '{ansible_pkg_mgr}',
+    'default'
+]
+
+DOCUMENTATION = """
+---
+module: dynamic_defaults
+author: "David Fischer (@davidfischer-ch)"
+short_description: Set variables based on operating system and package manager.
+description:
+  - Set variables to defaults using the lookup keys in lower-case and with / replaced by -.
+  - Set a variable dynamic_defaults_outputs to a list of (keys, resulting facts).
+options:
+  hostvars:
+    required: true
+    description:
+      - Simply set it to '{{ hostvars[inventory_hostname] }}'
+      - Required to retrieve dynamic defaults and set destination variables only if missing.
+  defaults:
+    required: true
+    description:
+      - A dictionary with defaults values.
+  lookup_keys:
+    required: false
+    default: %s
+    description:
+      - Keys used to retrieve defaults, order matters.
+  must_match:
+    required: false
+    default: false
+    description:
+      - Force that at least of the lookup keys is matched.
+""" % (LOOKUP_KEYS, )
+
+EXAMPLES = """
+    - name: Load dynamic defaults
+      dynamic_defaults:
+        hostvars: "{{ hostvars[inventory_hostname] }}"
+        defaults:
+          ubuntu:
+            web_package: nginx
+            python_package: python3
+
+          centos:
+            web_package: httpd
+            python_package: python3
+
+          default:
+            web_package: nginx
+            python_package: python
+
+    - name: Install web package
+      ansible.builtin.package:
+        name: "{{ web_package }}"
+        state: present
+
+    - name: Display selected variables
+      ansible.builtin.debug:
+        msg:
+          web_package: "{{ web_package }}"
+          python_package: "{{ python_package }}"
+"""
+
+
+def main():
+    # See https://newrelic.com/infrastructure/pricing (CPU Cores + GB RAM) x hours used
+    module = AnsibleModule(
+        argument_spec=dict(
+            hostvars=dict(required=True, type='dict'),  # NEVER use no_log otherwise it will break content!
+            defaults=dict(required=True, type='dict'),
+            lookup_keys=dict(required=False, default=LOOKUP_KEYS, type='list'),
+            must_match=dict(required=False, default=False, type='bool')
+        ),
+        supports_check_mode=True
+    )
+    hostvars, defaults, lookup_keys, must_match = (
+        module.params[k] for k in ('hostvars', 'defaults', 'lookup_keys', 'must_match')
+    )
+    try:
+        lookup_keys = tuple(clean_key(k.format(**hostvars)) for k in lookup_keys)
+    except AttributeError:
+        # Python 2.5 - 'str' object has no attribute 'format'
+        lookup_keys = tuple(
+            clean_key(k.replace('{', '%(').replace('}', ')s') % hostvars) for k in lookup_keys
+        )
+    facts, match = {}, False
+    for lookup_key in lookup_keys:
+        for key, variables in defaults.items():
+            if re.match('^%s$' % key, lookup_key):
+                match |= key != 'default'
+                for name, value in variables.items():
+                    if name not in hostvars:
+                        facts.setdefault(name, value)
+    if must_match and not match:
+        module.fail_json(msg='No match found, lookup keys %s.' % (lookup_keys, ))
+    outputs = facts.get('dynamic_defaults_outputs', [])
+    outputs.append([lookup_keys, facts.copy()])
+    facts['dynamic_defaults_outputs'] = outputs
+    module.exit_json(ansible_facts=facts, changed=False)
+
+
+def clean_key(key):
+    return key.replace('/', '-').replace(' ', '-').replace('"', '').lower()
+
+
+if __name__ == '__main__':
+    main()
---
+++ ansible/library/kernel_facts.py
@@ -0,0 +1,80 @@
+import errno, os, re, sys
+
+from ansible.module_utils.basic import AnsibleModule
+
+PY2 = sys.version_info[0] < 3
+if PY2:
+    import ConfigParser as configparser, io
+else:
+    import configparser
+
+DOCUMENTATION = r"""
+---
+module: kernel_facts
+author: "David Fischer (@davidfischer-ch)"
+short_description: Gather facts about the GNU/Linux Kernel configuration.
+options:
+  release:
+    required: false
+    default: null
+    description:
+      - Release to scan, defaults to active Kernel.
+"""
+
+EXAMPLES = r"""
+- name: Gather kernel facts
+  kernel_facts:
+
+- name: Check whether KVM is enabled in kernel
+  ansible.builtin.debug:
+    msg: "KVM kernel support is enabled"
+  when: ansible_facts.kernel_config.kvm | default('n') in ['y', 'm']
+
+- name: Check bridge netfilter
+  ansible.builtin.assert:
+    that:
+      - ansible_facts.kernel_config.bridge_netfilter | default('n') in ['y', 'm']
+    fail_msg: "Kernel does not support bridge netfilter"
+
+"""
+
+CONFIG_PREFIX = re.compile(r'^config_')
+DRIVER_IN_KERNEL = 'y'
+DRIVER_HAS_MODULE = 'm'
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            release=dict(default=None)
+        ),
+        supports_check_mode=True
+    )
+
+    module.exit_json(
+        changed=False,
+        ansible_facts=dict(
+            kernel_config=get_kernel_config(
+                module.params.get('release')
+            )
+        )
+    )
+
+
+
+def get_kernel_config(release=None):
+    # Using index on os.uname() to be retro-compatible with old python releases
+    try:
+        with open('/boot/config-{0}'.format(release or os.uname()[2])) as f:
+            config = configparser.ConfigParser()
+            config_string = '[kernel]' + f.read()
+            config.readfp(io.BytesIO(config_string)) if PY2 else config.read_string(config_string)
+    except IOError as e:
+        if e.errno == errno.ENOENT:
+            return {}
+        raise
+    return {CONFIG_PREFIX.sub('', k): v for k, v in config.items('kernel')}
+
+
+if __name__ == '__main__':
+    main()
